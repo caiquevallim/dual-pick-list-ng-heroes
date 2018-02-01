2c177223fb45ac3fa698f3c28303a203324b1d9c
Caique Vallim <caiquevallim@hotmail.com>
2018-02-01 00:04:56 -0200

    Create some others custom fields to use checkbox, select, button custom properties about dual pick list ng

--- src/ItemModel.ts
+++ src/ItemModel.ts
@@ -3,11 +3,15 @@ import {ItemDualPickListModel} from "dual-pick-list-ng";
 export class ItemModel extends ItemDualPickListModel{
   id:number;
   description:string;
+  isActive:boolean;
+  idPower:number;
 
-  constructor(id, description,selected = false){
+  constructor(id, description, isActive = true, idPower=null, selected = false){
     super();
     this.id = id;
     this.description = description;
+    this.isActive = isActive;
+    this.idPower = idPower;
 
     this.left = !selected;
     this.right = selected;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,23 +15,38 @@ export class AppComponent {
   leftList: Array<HeaderDualPickListModel>=[];
   rightList: Array<HeaderDualPickListModel>=[];
   headerConfig: HeaderModel;
+  powers:Array<any>=[];
   constructor(){
     this.items = [
-      new ItemModel(1, 'Batman'),
-      new ItemModel(2, 'Super-Man'),
-      new ItemModel(3, 'Robin'),
-      new ItemModel(4, 'Daredevil'),
-      new ItemModel(5, 'Cyclops')
+      new ItemModel(1, 'Batman', false, 1),
+      new ItemModel(2, 'Super-Man',true,2),
+      new ItemModel(3, 'Robin',true,3),
+      new ItemModel(4, 'Daredevil',true, 4),
+      new ItemModel(5, 'Cyclops',true, 0)
     ];
+
+    this.powers.push({id:1, text:"Fly"},{id:2, text:"Fire"}, {id:3, text:"Wind"},{id:4, text:"Water"});
 
     this.leftList = [
       new HeaderDualPickListModel('ID','id'),
       new HeaderDualPickListModel('Description','description'),
+      new HeaderDualPickListModel('Active', 'isActive', true, null, null, true),
+      new HeaderDualPickListModel('Powers', 'idPower',true,this.powers,null,null)
     ];
 
+    let btnFn = (item)=>{
+      if(!item.idPower || item.idPower <=0)
+        return;
+      let power = this.powers.find(it=>it.id == item.id);
+      if(power)
+        alert('The hero '+item.description+ ' has the '+power.text+' power');
+
+      item.isSelected = true;//if you wanna just click in the button and you wont need to select the item
+    };
     this.rightList = [
       new HeaderDualPickListModel('ID','id'),
       new HeaderDualPickListModel('Description','description'),
+      new HeaderDualPickListModel('What Power is it?','', true,null, btnFn )
     ];
 
     this.headerConfig = new HeaderModel(this.leftList, this.rightList);
